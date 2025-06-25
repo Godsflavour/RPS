3634f88334d2e42cb44e6e604822667061358a1b
Godsflavour <wgflavour@gmail.com>
2025-06-25 16:28:48 +0100

    fifi

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ let draw = 0;
 
 function play(userChoice) {
     const choices = ['rock', 'paper', 'scissors'];
-    const botChoice = [Math.floor(Math.random() * 3)];
+    const botChoice = choices[Math.floor(Math.random() * 3)];
 
    document.getElementById('user-choice').textContent = `Fifi: ${emoji(userChoice)}`;
-   document.getElementById('bot-choice').textContent = `Bot: ${emoji(userChoice)}`;
+   document.getElementById('bot-choice').textContent = `Bot: ${emoji(botChoice)}`;
 
-   const result = getresult('userchoice', 'botchoice');
+   const result = getresult(userchoice, botchoice);
    document.getElementById('outcome').textContent = result;
 
    if (result === "Fifi Wins!") {
     fifiScore;
-    document.getElementById('ScoreFifi').textContent = fifiScore;
    } else if(result === "Fifi lost!") {
     botScore;
    } else {
-    document.getElementById('ScoreDraw').textContent = drawScore;
-    drawScore;
+    draw;
    }
 }
 
@@ -41,3 +39,35 @@ function emoji(choice) {
     if (choice === 'paper') return '📃';
     if (choice === 'scissors') return '✂️';
 }
+
+function updateScores() {
+    document.getElementById('ScoreFifi').textContent = fifiScore;
+    document.getElementById('ScoreBot').textContent = botScore;
+    document.getElementById('ScoreDraw').textContent = drawScore;
+ }
+
+ function updateGameStatus(result) {
+    let status = document.getElementById('game-status');
+
+    if (result == "Fifi Wins!") {
+        status.innerText = "You won this round!";
+    }
+    if (result == "Fifi lost!") {
+        status.innerText = "Bot won this round!";
+    }
+    if (result == "It's a draw!") {
+        status.innerText = "It's a draw!";
+    }
+}
+
+function resetGame() {
+    fifiScore = 0;
+    botScore = 0;
+    drawScore = 0;
+    updateScores();
+    document.getElementById('user-choice').textContent = 'Fifi';
+    document.getElementById('bot-choice').textContent = 'Bot';
+    document.getElementById('outcome').textContent = '';
+    document.getElementById('game-status').textContent = "Fifi's turn";
+}
+
